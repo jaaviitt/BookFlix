499daac9fe073e361811b8dce414090459e2d5bf
jaaviith <javivi192003@gmail.com>
2026-02-09 17:09:52 +0100

    Panel del usuario en funcionamiento

--- bookReviews/src/main/java/com/trabajoFinal/bookReviews/controller/PerfilController.java
+++ bookReviews/src/main/java/com/trabajoFinal/bookReviews/controller/PerfilController.java
@@ -7,9 +7,12 @@ import com.trabajoFinal.bookReviews.repository.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -22,13 +25,17 @@ public class PerfilController {
     @Autowired
     private ResenaRepository resenaRepository;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    // --- 1. VER PERFIL (GET) ---
     @GetMapping("/perfil")
     public String verMiPerfil(Model model, @AuthenticationPrincipal UserDetails userDetails) {
         if (userDetails == null) {
             return "redirect:/login";
         }
 
-        // 1. Buscamos al usuario logueado en la BD
+        // 1. Buscamos al usuario logueado
         Usuario usuario = usuarioRepository.findByUsername(userDetails.getUsername()).orElse(null);
 
         if (usuario != null) {
@@ -42,5 +49,30 @@ public class PerfilController {
         }
 
         return "perfil"; // Carga perfil.html
+    }
+
+    // --- 2. GUARDAR CAMBIOS (POST) ---
+    @PostMapping("/perfil/guardar")
+    public String actualizarPerfil(@RequestParam String email,
+                                   @RequestParam(required = false) String newPassword,
+                                   @AuthenticationPrincipal UserDetails userDetails) {
+
+        if (userDetails == null) return "redirect:/login";
+
+        Usuario usuario = usuarioRepository.findByUsername(userDetails.getUsername()).orElse(null);
+
+        if (usuario != null) {
+            // Actualizar Email
+            usuario.setEmail(email);
+
+            // Actualizar Contraseña (SOLO si el usuario escribió algo nuevo)
+            if (newPassword != null && !newPassword.trim().isEmpty()) {
+                usuario.setPassword(passwordEncoder.encode(newPassword));
+            }
+
+            usuarioRepository.save(usuario);
+        }
+
+        return "redirect:/perfil?exito";
     }
 }
